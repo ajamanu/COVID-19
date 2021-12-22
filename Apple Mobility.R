bd69682f2c8e7392846ac99964cb3ff3e5b9c236
ajamanu <amanu@champequity.com>
2021-12-22 14:19:20 +1100

    Updated chart

--- Apple Mobility.R
+++ Apple Mobility.R
@@ -74,11 +74,12 @@ data1 <- data1 %>%
 data1 %>% 
         filter(region %in% c("Australia", "Sydney", "Melbourne", "Brisbane", "Perth")) %>% 
         ggplot(aes(date, index, colour = region)) +
-        geom_smooth(se = FALSE) +
+        geom_smooth(se = FALSE, span = 0.35) +
         geom_hline(yintercept = 100, colour = "red", linetype = "dashed") +
         geom_vline(xintercept = as.Date("2020-03-21"), colour = "red", linetype = "dashed") +
         geom_vline(xintercept = as.Date("2020-07-09"), colour = "red", linetype = "dashed") +
-        scale_x_date(date_labels = "%b-%Y") +
+        scale_x_date(date_labels = "%b-%y") +
+        scale_color_brewer(palette = "Pastel1") +
         facet_wrap(~transportation_type) +
         labs(title = paste("Mobility Data for Selected Australian Cities as at", 
                            format(max(data1$date), "%d %B")),
@@ -98,8 +99,9 @@ data1 %>%
         filter(region %in% c("Sydney", "London", "Stockholm", "Tokyo", "Auckland",
                              "New York City")) %>% 
         ggplot(aes(date, index, colour = region)) +
-        geom_smooth(se = FALSE) +
+        geom_smooth(se = FALSE, span = 0.4) +
         geom_hline(yintercept = 100, colour = "red", linetype = "dashed") +
+        scale_x_date(date_labels = "%b-%y") +
         facet_wrap(~transportation_type) +
         labs(title = paste("Mobility Data for Selected Global Cities as at", 
                            format(max(data1$date), "%d %B")),
@@ -107,15 +109,17 @@ data1 %>%
              y = "Index (100 = Baseline)",
              caption = "Source: https://www.apple.com/covid19/mobility") +
         theme_minimal() +
-        theme(legend.title = element_blank())
+        theme(legend.title = element_blank(),
+              axis.text.x = element_text(angle = 45))
 
 ### Country
 data1 %>% 
         filter(region %in% c("Australia", "United Kingdom", "Germany", "Japan", "New Zealand",
                              "United States")) %>% 
         ggplot(aes(date, index, colour = region)) +
-        geom_smooth(se = FALSE) +
+        geom_smooth(se = FALSE, span = 0.4) +
         geom_hline(yintercept = 100, colour = "red", linetype = "dashed") +
+        scale_x_date(date_labels = "%b-%y") +
         facet_wrap(~transportation_type) +
         labs(title = paste("Mobility Data for Selected Global Cities as at", 
                            format(max(data1$date), "%d %B")),
@@ -123,4 +127,5 @@ data1 %>%
              y = "Index (100 = Baseline)",
              caption = "Source: https://www.apple.com/covid19/mobility") +
         theme_minimal() +
-        theme(legend.title = element_blank())
+        theme(legend.title = element_blank(),
+              axis.text.x = element_text(angle = 45))
